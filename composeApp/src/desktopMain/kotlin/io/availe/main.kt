7cc5cf2e18364f402384fee81596d6548278b229
availe <94802525+availe@users.noreply.github.com>
2025-05-02 15:15:41 -0400

    project init

---
+++ composeApp/src/desktopMain/kotlin/io/availe/main.kt
@@ -0,0 +1,13 @@
+package io.availe
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "availe",
+    ) {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/io/availe/Constants.kt
@@ -0,0 +1,3 @@
+package io.availe
+
+const val SERVER_PORT = 8080
