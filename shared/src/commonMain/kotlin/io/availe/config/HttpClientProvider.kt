3acc42a8c8dccdd159e1c99f149e4eaf40ae7393
availe <94802525+availe@users.noreply.github.com>
2025-06-03 19:58:48 -0400

    Update HttpClientProvider.kt

--- shared/src/commonMain/kotlin/io/availe/config/HttpClientProvider.kt
+++ shared/src/commonMain/kotlin/io/availe/config/HttpClientProvider.kt
@@ -1,10 +1,15 @@
 package io.availe.config
 
+import IChatService
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.rpc.krpc.ktor.client.installKrpc
+import kotlinx.rpc.krpc.ktor.client.rpc
+import kotlinx.rpc.krpc.serialization.json.json
+import kotlinx.rpc.withService
+import kotlinx.serialization.json.Json
 
 expect val httpClientEngine: HttpClientEngineFactory<*>
 
@@ -15,12 +20,17 @@ object HttpClientProvider {
         }
     }
 
-    val rpcClient: HttpClient = HttpClient(httpClientEngine) {
-        install(ContentNegotiation) {
-            json()
-        }
+    val krpcClient: HttpClient = HttpClient(httpClientEngine) {
         installKrpc {
-            waitForServices = true
+            serialization {
+                json(Json { prettyPrint = true })
+            }
         }
+    }
+
+    suspend fun createChatService(): IChatService {
+        val rawRpc = krpcClient.rpc("${NetworkConfig.serverUrl}/krpc/chat") {
+        }
+        return rawRpc.withService<IChatService>()
     }
 }
