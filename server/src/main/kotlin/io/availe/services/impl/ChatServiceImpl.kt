a57826c1878c8b92648b7a1d27595ac573d486e4
availe <94802525+availe@users.noreply.github.com>
2025-06-03 18:31:27 -0400

    Update ChatServiceImpl.kt

--- server/src/main/kotlin/io/availe/services/impl/ChatServiceImpl.kt
+++ server/src/main/kotlin/io/availe/services/impl/ChatServiceImpl.kt
@@ -2,17 +2,26 @@ package io.availe.services.impl
 
 import IChatService
 import arrow.core.Either
+import arrow.core.raise.either
 import io.availe.models.Session
 import io.availe.services.ApiError
+import io.availe.services.ChatError
 import io.availe.services.ChatStore
+import io.availe.services.toApiError
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
-object ChatServiceImpl : IChatService {
-    override suspend fun getAllSessions(request: Unit): Either<ApiError, List<Session>> {
-        val id = ChatStore.getAllSessionIdentifiers()
 
-        TODO("Not yet implemented")
+object ChatServiceImpl : IChatService {
+    override suspend fun getAllSessions(request: Unit): Either<ApiError, List<Session>> = either {
+        val sessionIds: List<String> = ChatStore.getAllSessionIdentifiers()
+
+        sessionIds.map { sessionId ->
+            ChatStore
+                .getSession(sessionId)
+                .mapLeft { chatError: ChatError -> chatError.toApiError() }
+                .bind()
+        }
     }
 
     override val coroutineContext: CoroutineContext = Dispatchers.Default
