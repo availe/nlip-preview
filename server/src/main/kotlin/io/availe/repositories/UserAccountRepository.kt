a410681c46aeab9debd3cdb94611ef5488cd3c42
availe <94802525+availe@users.noreply.github.com>
2025-06-08 17:35:48 -0400

    user account repo

--- server/src/main/kotlin/io/availe/repositories/UserAccountRepository.kt
+++ server/src/main/kotlin/io/availe/repositories/UserAccountRepository.kt
@@ -2,15 +2,14 @@
 
 package io.availe.repositories
 
-import arrow.core.Either
-import arrow.core.None
-import arrow.core.Option
-import arrow.core.raise.either
-import arrow.core.some
+import arrow.core.*
 import io.availe.jooq.enums.UserSubscriptionTierEnum
 import io.availe.jooq.tables.UserAccounts
 import io.availe.models.*
 import org.jooq.DSLContext
+import org.jooq.Field
+import org.jooq.exception.DataAccessException
+import org.postgresql.util.PSQLState
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.toJavaUuid
 import kotlin.uuid.toKotlinUuid
@@ -40,8 +39,70 @@ class UserAccountRepository(private val dsl: DSLContext) {
         ).some()
     }
 
-    internal fun insert(user: UserAccount): Either<UserAccountError, UserAccount> = either {
-        TODO()
+    internal fun insertUserAccount(userAccount: UserAccountCreate): Either<UserAccountError, UserAccount> {
+        try {
+            val record = dsl
+                .insertInto(UserAccounts.USER_ACCOUNTS)
+                .set(UserAccounts.USER_ACCOUNTS.USERNAME, userAccount.username.value)
+                .set(UserAccounts.USER_ACCOUNTS.EMAIL_ADDRESS, userAccount.emailAddress.value)
+                .set(UserAccounts.USER_ACCOUNTS.ACCOUNT_IS_ACTIVE, userAccount.accountIsActive.value)
+                .set(UserAccounts.USER_ACCOUNTS.SUBSCRIPTION_TIER, userAccount.subscriptionTier.toJooq())
+                .set(UserAccounts.USER_ACCOUNTS.SCHEMA_VERSION, userAccount.schemaVersion.value)
+                .returning(
+                    UserAccounts.USER_ACCOUNTS.ID,
+                    UserAccounts.USER_ACCOUNTS.USERNAME,
+                    UserAccounts.USER_ACCOUNTS.EMAIL_ADDRESS,
+                    UserAccounts.USER_ACCOUNTS.ACCOUNT_IS_ACTIVE,
+                    UserAccounts.USER_ACCOUNTS.SUBSCRIPTION_TIER,
+                    UserAccounts.USER_ACCOUNTS.SCHEMA_VERSION,
+                ).fetchOne()
+
+            return UserAccount(
+                id = UserAccountId(checkNotNull(checkNotNull(record).id).toKotlinUuid()),
+                username = Username(checkNotNull(record).username),
+                emailAddress = EmailAddress(checkNotNull(record).emailAddress),
+                accountIsActive = AccountIsActive(record.accountIsActive),
+                subscriptionTier = record.subscriptionTier.toModel(),
+                schemaVersion = UserAccountSchemaVersion(record.schemaVersion),
+            ).right()
+        } catch (e: DataAccessException) {
+            if (e.sqlState() == PSQLState.UNIQUE_VIOLATION.state) {
+                return UserAccountError.UserAlreadyExists.left()
+            }
+            throw e
+        }
+    }
+
+    internal fun deleteUserAccount(userAccountId: UserAccountId): Option<Unit> {
+        val rowsDeleted: Int = dsl
+            .deleteFrom(UserAccounts.USER_ACCOUNTS)
+            .where(UserAccounts.USER_ACCOUNTS.ID.eq(userAccountId.value.toJavaUuid()))
+            .execute()
+
+        return if (rowsDeleted > 0) Unit.some() else None
+    }
+
+    internal fun patchUserAccount(userAccountId: UserAccountId, patch: UserAccountPatch): Option<Unit> {
+        val query = mutableMapOf<Field<*>, Any>()
+        if (patch.accountIsActive.isSome()) {
+            query[UserAccounts.USER_ACCOUNTS.ACCOUNT_IS_ACTIVE] = patch.accountIsActive
+        }
+        if (patch.emailAddress.isSome()) {
+            query[UserAccounts.USER_ACCOUNTS.EMAIL_ADDRESS] = patch.emailAddress
+        }
+        if (patch.accountIsActive.isSome()) {
+            query[UserAccounts.USER_ACCOUNTS.SUBSCRIPTION_TIER] = patch.subscriptionTier.map { it.toJooq() }
+        }
+
+        if (query.isEmpty()) return None
+
+        val rowsUpdated: Int = dsl
+            .update(UserAccounts.USER_ACCOUNTS)
+            .set(query)
+            .where(UserAccounts.USER_ACCOUNTS.ID.eq(userAccountId.value.toJavaUuid()))
+            .execute()
+
+        return if (rowsUpdated > 0) Unit.some() else None
     }
 }
 
