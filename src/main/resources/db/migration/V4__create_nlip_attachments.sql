c8647ee1b36945cb3cd670dcf10e3b4e6951e075
availe <94802525+availe@users.noreply.github.com>
2025-06-09 16:39:21 -0400

    remove deferred

--- src/main/resources/db/migration/V4__create_nlip_attachments.sql
+++ src/main/resources/db/migration/V4__create_nlip_attachments.sql
@@ -13,9 +13,9 @@ CREATE TABLE nlip_attachments
             OR (nlip_message_id IS NULL AND nlip_submessage_id IS NOT NULL)
         ),
     CONSTRAINT uq_nlip_attachments_message_file
-        UNIQUE (nlip_message_id, file_key) DEFERRABLE INITIALLY DEFERRED,
+        UNIQUE (nlip_message_id, file_key),
     CONSTRAINT uq_nlip_attachments_submessage_file
-        UNIQUE (nlip_submessage_id, file_key) DEFERRABLE INITIALLY DEFERRED
+        UNIQUE (nlip_submessage_id, file_key)
 );
 
 CREATE INDEX index_nlip_attachments_message_id ON nlip_attachments (nlip_message_id);
