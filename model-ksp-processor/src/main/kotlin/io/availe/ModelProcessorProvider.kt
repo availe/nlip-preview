03c73aa351129cc1d75db3b4b8063f95eaa1adc9
availe <94802525+availe@users.noreply.github.com>
2025-06-17 18:08:25 -0400

    remove kreplica files as plugin will be used from now on

--- model-ksp-processor/src/main/kotlin/io/availe/ModelProcessorProvider.kt
+++
@@ -1,8 +0,0 @@
-package io.availe
-
-import com.google.devtools.ksp.processing.*
-
-class ModelProcessorProvider : SymbolProcessorProvider {
-    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor =
-        ModelProcessor(environment)
-}
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -77,11 +77,6 @@ application {
 }
 
 dependencies {
-    ksp(project(":model-ksp-processor"))
-    ksp(project(":model-ksp-annotations"))
-    ksp(project(":codegen"))
-    implementation(projects.codegen)
-    implementation(project(":model-ksp-annotations"))
     implementation(projects.shared)
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
@@ -234,37 +229,4 @@ tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJ
 tasks.named("generateJooq") {
     dependsOn("compileJooqKotlin")
     dependsOn("flywayMigrate")
-}
-
-val codegen by configurations.creating
-
-dependencies {
-    // Add the codegen-runtime project to our new configuration
-    codegen(project(":codegen-runtime"))
-}
-
-tasks.register<JavaExec>("runCodegen") {
-    group = "build"
-    description = "Runs the KSP-based code generator"
-    dependsOn(tasks.named("kspKotlin"), project(":shared").tasks.named("kspCommonMainKotlinMetadata"))
-
-    mainClass.set("io.availe.ApplicationKt")
-    classpath = codegen
-
-    val serverJsonFile = layout.buildDirectory.file("generated/ksp/main/resources/models.json")
-    val sharedJsonFile =
-        project(":shared").layout.buildDirectory.file("generated/ksp/metadata/commonMain/resources/models.json")
-
-    args(serverJsonFile.get().asFile.absolutePath, sharedJsonFile.get().asFile.absolutePath)
-}
-
-// All compilation tasks must wait for the codegen to finish generating sources
-tasks.withType<KotlinCompilationTask<*>>().configureEach {
-    if (name == "compileKotlin") {
-        dependsOn(tasks.named("runCodegen"))
-    }
-}
-
-kotlin.sourceSets.named("main") {
-    kotlin.srcDir(layout.buildDirectory.dir("generated/kotlin-poet"))
 }
